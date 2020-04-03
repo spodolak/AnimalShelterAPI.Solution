90fc0e065c7834c045e7c89f8d9dd11e23034941
Stephanie Podolak <StephPodolak@gmail.com>
2020-04-03 13:43:18 -0700

    add validation errors to cat and dog models

--- AnimalShelter/Models/Cat.cs
+++ AnimalShelter/Models/Cat.cs
@@ -1,11 +1,15 @@
 namespace AnimalShelter.Models
 {
-    public class Cat
-    {
-        public int CatId { get; set; }
-        public string Name { get; set; }
-        public string Traits { get; set; }
-        public int Age { get; set; }
-        public string Gender { get; set; }
-    }
+  public class Cat
+  {
+    public int CatId { get; set; }
+    [Required]
+    [StringLength(20)]
+    public string Name { get; set; }
+    public string Traits { get; set; }
+    [Required]
+    [Range(0, 35, ErrorMessage = "Age must be between 0 and 35.")]
+    public int Age { get; set; }
+    public string Gender { get; set; }
+  }
 }
