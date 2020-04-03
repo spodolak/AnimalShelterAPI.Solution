01f391cfe00c0a8a916e3c9cd4911b142b0086fb
Stephanie Podolak <StephPodolak@gmail.com>
2020-04-03 15:42:03 -0700

    update AnimalShelter.csproj and Startup.cs to install NSwag interface

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -9,45 +9,51 @@ using Microsoft.Extensions.DependencyInjection;
 namespace AnimalShelter
 {
   public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-          services.AddDbContext<AnimalShelterContext>(opt =>
-              opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
-          services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-          services.AddApiVersioning(config =>
-          {
-              // Specify the default API Version as 1.0
-              config.DefaultApiVersion = new ApiVersion(2, 1);
-              // If the client hasn't specified the API version in the request, use the default API version number 
-              config.AssumeDefaultVersionWhenUnspecified = true;
-              config.ReportApiVersions = true;
-          });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            // app.UseHttpsRedirection();
-            app.UseMvc();
-        }
+        Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<AnimalShelterContext>(opt =>
+        opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      services.AddApiVersioning(config =>
+      {
+        // Specify the default API Version as 1.0
+        config.DefaultApiVersion = new ApiVersion(2, 1);
+        // If the client hasn't specified the API version in the request, use the default API version number 
+        config.AssumeDefaultVersionWhenUnspecified = true;
+        config.ReportApiVersions = true;
+      });
+      // Register the Swagger services
+      services.AddSwaggerDocument();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      // Register the Swagger generator and the Swagger UI middlewares
+      app.UseOpenApi();
+      app.UseSwaggerUi3();
+
+      // app.UseHttpsRedirection();
+      app.UseMvc();
+    }
+  }
 }
