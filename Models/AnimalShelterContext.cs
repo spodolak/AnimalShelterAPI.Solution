e315b9abe713dc326903d6c920e310648bcc7b47
Stephanie Podolak <StephPodolak@gmail.com>
2020-04-03 08:58:45 -0700

    initialize project boilerplate directories

---
+++ Models/AnimalShelterContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Models
+{
+    public class AnimalShelterContext : DbContext
+    {
+        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cat> Cats { get; set; }
+        public DbSet<Dog> Dogs { get; set; }
+    }
+}
